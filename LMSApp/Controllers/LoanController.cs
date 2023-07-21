b1beb41080ecb99953fdb3b91d016f9c65fb332d
Administrator <Administrator@854CD7B6B5A4514>
2023-07-21 16:46:51 +0530

    commit changes

--- LMSApp/Controllers/LoanController.cs
+++ LMSApp/Controllers/LoanController.cs
@@ -1,6 +1,7 @@
 ﻿using LMSApp.DBContext;
 using LMSApp.Interface;
 using LMSApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,6 +10,7 @@ namespace LMSApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class LoanController : ControllerBase
     {
         private UserAccessDBContext _dBContext;
